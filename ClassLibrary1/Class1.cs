621cca45c5f908cc1d62b2103375374bc6e2863c
badcel <1218031+badcel@users.noreply.github.com>
2023-02-21 16:16:52 +0100

    Initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassLibrary1;
+
+public interface IMyInterface
+{
+    string Value { get; }
+}
+
+public abstract class BaseClass<T>
+{
+    public T Value { get; }
+
+    protected BaseClass(T value)
+    {
+        Value = value;
+    }
+}
+
+//No warning with "dotnet build". Rider reports for "IMyInterface":
+//Nullablility of return type does not match implicitly implemented member
+//'string ClassLibrary1.IMyInterface.Vlaue.get' (possibly because of
+//nullability attributes)
+public class MyClass : BaseClass<string>, IMyInterface
+{
+    public MyClass(string value) : base(value)
+    {
+    }
+}
